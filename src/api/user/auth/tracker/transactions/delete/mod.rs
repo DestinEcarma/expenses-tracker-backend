5031410743872cd0a7e7120d052c26e36746640e
MakerDoe <makerdoe09@gmail.com>
2024-05-24 04:35:35 +0800

    Update mod.rs
    Transfered transaction_ownership to it's own middleware

--- src/api/user/auth/tracker/transactions/delete/mod.rs
+++ src/api/user/auth/tracker/transactions/delete/mod.rs
@@ -1,7 +1,6 @@
 mod utils;
 
 use self::utils::delete_transaction;
-use super::utils::transaction_ownership;
 use crate::{
 	api::db::defs::{DBTables, ExtensionDB},
 	error::Result,
@@ -11,12 +10,10 @@ use surrealdb::sql::Thing;
 
 pub async fn handler(
 	db: ExtensionDB,
-	Path((category_id, transaction_id)): Path<(String, String)>,
+	Path((_, transaction_id)): Path<(String, String)>,
 ) -> Result<impl IntoResponse> {
-	let category_id = Thing::from((DBTables::CATEGORY, category_id.as_str()));
 	let transaction_id = Thing::from((DBTables::TRANSACTION, transaction_id.as_str()));
 
-	transaction_ownership(&db, &category_id, &transaction_id).await?;
 	delete_transaction(&db, &transaction_id).await?;
 
 	Ok(StatusCode::OK)
