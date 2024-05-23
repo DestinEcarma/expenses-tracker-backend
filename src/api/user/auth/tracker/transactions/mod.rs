8e20017f93013f9769068dd6b8f342424895bd97
MakerDoe <makerdoe09@gmail.com>
2024-05-24 04:36:26 +0800

    Update mod.rs
    Modified the router to handle it's own Path
    Transferred mw_category_ownership to utils.mod

--- src/api/user/auth/tracker/transactions/mod.rs
+++ src/api/user/auth/tracker/transactions/mod.rs
@@ -3,40 +3,19 @@ mod get;
 mod post;
 mod utils;
 
-use super::utils::category_ownership;
-use crate::{
-	api::{
-		db::defs::{DBTables, ExtensionDB},
-		user::auth::ctx::UserId,
-	},
-	error::Result,
-};
-use axum::{
-	extract::{Path, Request},
-	middleware::{self, Next},
-	response::Response,
-	routing, Router,
-};
-use surrealdb::sql::Thing;
+use self::utils::{mw_category_ownership, mw_transaction_ownership};
+use axum::{middleware, routing, Router};
 
-#[rustfmt::skip]
 pub fn router() -> Router {
 	Router::new()
-		.route("/", routing::get(get::handler).post(post::handler))
-		.route("/:id", routing::delete(delete::handler))
-		.route_layer(middleware::from_fn(mw_category_ownership))
-}
-
-async fn mw_category_ownership(
-	db: ExtensionDB,
-	ctx: UserId,
-	Path(category_id): Path<String>,
-	req: Request,
-	next: Next,
-) -> Result<Response> {
-	let category_id = Thing::from((DBTables::CATEGORY, category_id.as_str()));
-
-	category_ownership(&db, ctx.id(), &category_id).await?;
-
-	Ok(next.run(req).await)
+		.merge(
+			Router::new()
+				.route("/", routing::get(get::handler).post(post::handler))
+				.route_layer(middleware::from_fn(mw_category_ownership)),
+		)
+		.merge(
+			Router::new()
+				.route("/:transaction_id", routing::delete(delete::handler))
+				.route_layer(middleware::from_fn(mw_transaction_ownership)),
+		)
 }
