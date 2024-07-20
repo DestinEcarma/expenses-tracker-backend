c77539bc2ec0fc70000676c9431accc107cc782d
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-21 05:32:05 +0800

    Update src/api/user/auth/tracker/categories/get/mod.rs
    Refactor: get_categories was removed

--- src/api/user/auth/tracker/categories/get/mod.rs
+++ src/api/user/auth/tracker/categories/get/mod.rs
@@ -1,12 +1,29 @@
-mod utils;
-
-use self::utils::get_categories;
 use crate::{
-	api::{db::defs::ExtensionDB, user::auth::ctx::UserId},
+	api::{
+		db::defs::{DBGlobalQuery, ExtensionDB},
+		user::auth::ctx::UserId,
+	},
 	error::Result,
 };
 use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
+use surrealdb::sql::Thing;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Category {
+	id: Thing,
+	name: String,
+	icon: String,
+	amount: f64,
+	transactions: usize,
+}
 
 pub async fn handler(db: ExtensionDB, ctx: UserId) -> Result<impl IntoResponse> {
-	Ok((StatusCode::OK, Json(get_categories(&db, ctx.id()).await?)))
+	let categories = db
+		.query(DBGlobalQuery::SELECT_USER_CATEGORIES)
+		.bind(("user_id", ctx.id()))
+		.await?
+		.take::<Vec<Category>>(0)?;
+
+	Ok((StatusCode::OK, Json(categories)))
 }
