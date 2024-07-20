45f0b51119e38a16f8cf6d6f70839e3490f428d7
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-07-21 05:33:13 +0800

    Update src/api/defs.rs
    Removed: `RecordOut` struct
    Removed: `merge_json` function

--- src/api/defs.rs
+++ src/api/defs.rs
@@ -1,5 +1,4 @@
-use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
+use serde::Deserialize;
 use surrealdb::sql::Thing;
 
 #[derive(Debug, Deserialize)]
@@ -13,33 +12,8 @@ impl Record {
 	}
 }
 
-#[derive(Debug, Deserialize)]
-pub struct RecordOut {
-	out: Vec<Thing>,
-}
-
-impl RecordOut {
-	pub fn out(&self) -> &Vec<Thing> {
-		&self.out
-	}
-}
-
 pub struct CookieNames;
 
 impl CookieNames {
 	pub const AUTH_TOKEN: &'static str = "auth-token";
-}
-
-pub fn merge_json<T: Serialize, S: Serialize>(value1: T, value2: S) -> Value {
-	let mut merged = json!(value1);
-
-	if let Value::Object(map1) = &mut merged {
-		if let Value::Object(map2) = json!(value2) {
-			for (k, v) in map2 {
-				map1.insert(k, v);
-			}
-		}
-	}
-
-	merged
 }
